a90479a843aab18cd9cf01cabb7d7293d15682b0
Maxim Kozlov <Maximusvin@gmail.com>
2021-03-21 22:12:02 +0200

    finish

--- main.js
+++ main.js
@@ -29,3 +29,65 @@ document.addEventListener("keydown", (e) => {
     }
   }
 });
+
+// ==========================
+
+// 2. Повісити івент лісенер на ресайз сторінки, коли розмір сторінки буде менше 600px,
+// виводити лог ми векористовуємо мобільну версію,
+// при більшому розмірі сторінки - що ми використовуємо десктоп версію.
+
+const task2Ref = document.querySelector(".task2");
+
+window.addEventListener("resize", () => {
+  if (innerWidth < 600) {
+    task2Ref.textContent = "Ви використовуєте мобільну версію";
+  }
+  if (innerWidth > 600) {
+    task2Ref.textContent = "Ви використовуєте десктопну версію";
+  }
+});
+
+// 3. Повісити івент на інпут, при зміні стану інпута - виводити повідомленням зі значенням яке зараз є введено.
+const inputRef3 = document.querySelector(".t3");
+
+inputRef3.addEventListener("blur", () => {
+  const message = inputRef3.value;
+
+  if (message.trim()) {
+    inputRef3.value = "";
+    alert(`Ви ввели: ${message}`);
+  } else {
+    alert("Введіть текст в поле");
+  }
+});
+
+// 4. При натисканні на кнопку альт+ о виводити модальне вікно на екран, альт + с - закривати.
+
+const modalRef = document.querySelector(".backdrop");
+
+document.addEventListener("keydown", (e) => {
+  if (e.altKey && e.code == "KeyO") {
+    modalRef.classList.add("active");
+  }
+
+  if (e.altKey && e.code == "KeyC") {
+    modalRef.classList.remove("active");
+  }
+});
+
+// 5. Створити модальне відкно аналог Алерта але з гарним дизайном.
+// по кліку на кнопку зявляється модальне вікно по центрі екрану(центрування елемента).
+// В модальному вікні має бути текст та кнопка закрити.
+
+const btnRef = document.querySelector("#btn");
+const modalRef2 = document.querySelector(".backdrop2");
+
+btnRef.addEventListener("click", () => {
+  modalRef2.classList.add("active");
+});
+
+modalRef2.addEventListener("click", (e) => {
+  if (e.target.nodeName === "BUTTON") {
+    modalRef2.classList.remove("active");
+  }
+});
